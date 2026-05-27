66fff3f8620291bcf4da49f1be2d4594a45ff239
WredHarris <wredharris57@gmail.com>
2026-05-26 20:45:32 -0600

    fixed stuttering

---
+++ src/kernel.c
@@ -0,0 +1,10 @@
+//
+// Created by wred-harris on 5/26/26.
+//
+
+#include "kernel.h"
+
+void kernel_main()
+{
+   
+}
---
+++ src/kernel.h
@@ -0,0 +1,10 @@
+//
+// Created by wred-harris on 5/26/26.
+//
+
+#ifndef KERNEL_H
+#define KERNEL_H
+
+void kernel_main();
+
+#endif //CARBON_OS_KERNEL_H
