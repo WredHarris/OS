eb1ed3c94fa3834b8975b2d30e7a6bdf843ca404
WredHarris <wredharris57@gmail.com>
2026-05-30 11:17:10 -0600

    implemented heap and malloc function

---
+++ src/memory/heap/kheap.c
@@ -0,0 +1,24 @@
+#include "kheap.h"
+#include "heap.h"
+#include "config.h"
+#include "kernel.h"
+
+
+struct heap kernel_heap;
+struct heap_table kernel_heap_table;
+
+
+void kheap_init()
+{
+    int total_heap_table_entries = OS_TOTAL_HEAP_SIZE_BTYES / OS_HEAP_BLOCK_SIZE;
+    kernel_heap_table.entries = (HEAP_BLOCK_TABLE_ENTRY*)(OS_HEAP_TABLE_ADDRESS);
+    kernel_heap_table.total = total_heap_table_entries;
+
+    void* end = (void*)(OS_HEAP_ADDRESS + OS_TOTAL_HEAP_SIZE_BTYES);
+
+    int res = heap_create(&kernel_heap, (void*)(OS_HEAP_ADDRESS), end, &kernel_heap_table);
+
+    if (res < 0) {
+        print("Failed to create heap\n");
+    }
+}
